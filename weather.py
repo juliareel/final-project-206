0f3af0e9cb886721410ae38e4a01e58f246d038c
ludeerin <89855207+ludeerin@users.noreply.github.com>
2021-12-07 17:31:24 -0500

    added documentation

--- weather.py
+++ weather.py
@@ -16,6 +16,13 @@ for day in range(number_of_days):
 
 
 def create_request_url(county, list_dates):
+    """
+    Takes in a list of dates and a county name (string) as an input. Generates a url to search within the weather api using a base url in tandem with 
+    parameters which are formed using the input values. It then indexes the json data returned by the search, and appends the 'historical' values of each 
+    dictionary to a list, 'list_data'. It then returns this list of data, which is a list of dictionaries where the keys are the dates for every day in the year
+    2019 and the values contain the information we need for our analysis.
+    
+    """
     list_data = []
     for date in list_dates:
         base_url = 'https://api.weatherstack.com/historical?access_key=c1e596fa44c50dbf9597860a4e84a937'
@@ -31,6 +38,12 @@ def create_request_url(county, list_dates):
 
 
 def snow_data(data):
+    """
+    Takes in a a dictionary and iterates through the keys which in our case are dates of each day in a year, and adds the total snowfall per day
+    to a variable by indexing into the dictionary and accessing the total snowfall per day. It then returns that number, which in our case is the 
+    total snow fall in a year for a specific county.
+
+    """
     snow_dict = {}
     total_snow = 0
     list_dates = data.keys()
@@ -45,6 +58,13 @@ def snow_data(data):
     
 
 def temp_data(data):
+    """
+    Takes in a dictionary and iterates through the keys which in our case are dates of each day in a year, and adds the avg temperature per day
+    to a variable by indexing into the dictionary and accessing the total snowfall per day. It then divides that number by the amount of items in
+    dictionary, which in our case is 365 (because there were 365 days int he year 2019) returns that number, which in our case is the 
+    average temparature for the entire year of 2019 for a specific county.
+
+    """
     avg_temp_dict= {}
     total_temp = 0
     list_dates = data.keys()
@@ -61,6 +81,12 @@ def temp_data(data):
 
 
 def snow_per_county(list_counties):
+    """
+    Takes in a list of counties (a list of strings) as input and, using create_request_url, tailors the api to search for data on each county by iterating 
+    through the list. It then uses snow_data to get the total snowfall for each county. It saves each county name and total snowfall for the year 
+    2019 in inches to a dictionary where the keys are county names and the values are the total snowfall in inches for the given county.
+    
+    """
     snow_county_dict = {}
     for county in list_counties:
         total_snow = 0
@@ -71,6 +97,12 @@ def snow_per_county(list_counties):
     return snow_county_dict
 
 def temp_per_county(list_counties):
+    """
+    Takes in a list of counties (a list of strings) as input and, using create_request_url, tailors the api to search for data on each county by iterating 
+    through the list. It then uses temp_data to get the avg yearly temp for each county. It saves each county name and total snowfall for the year 
+    2019 in inches to a dictionary where the keys are county names and the values are the total snowfall in inches for the given county.
+    
+    """
     temp_county_dict = {}
     for county in list_counties:
         total_temp = 0
@@ -84,6 +116,11 @@ def temp_per_county(list_counties):
 
 
 def create_county_list(cur, conn):
+    """
+    This function generates a list of counties by pulling each county name from the "Counties" table in the database using a select statement. It 
+    then appends each name to a list by indexing the tuples returned with the fetchall statement. 
+    
+    """
     county_list = []
     cur.execute('SELECT county FROM Counties')
     conn.commit()
@@ -94,6 +131,11 @@ def create_county_list(cur, conn):
     return county_list
 
 def write_snow_cache(CACHE_FNAME, list_counties):
+    """
+    This function takes a string and a list as inputs. The string is what you want to name the file you are going to write the data to. It then
+    writes the dictionary generated from snow_per_county to a json file with the name of your choice.
+    
+    """
     fw = open(CACHE_FNAME, "w")
 
     dicto = json.dumps(snow_per_county(list_counties))
@@ -103,6 +145,12 @@ def write_snow_cache(CACHE_FNAME, list_counties):
     fw.close()
 
 def write_temp_cache(CACHE_FNAME, list_counties):
+    """
+    This function takes a string and a list as inputs. The string is what you want to name the file you are going to write the data to. It then
+    writes the dictionary generated from plugging the list of counties into the function temp_per_county to a json file with the file name of your
+    choice.
+    
+    """
     fw = open(CACHE_FNAME, "w")
 
     dicto = json.dumps(temp_per_county(list_counties))
